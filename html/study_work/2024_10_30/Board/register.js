61c18af6741add7c5475893f20b20fdeb1b41545
chan237 <bak7771@naver.com>
2024-11-01 08:00:18 +0900

    [append]register.html, register.js, register.css 패턴추가 및 오류수정 1차

--- html/study_work/2024_10_30/Board/register.js
+++ html/study_work/2024_10_30/Board/register.js
@@ -1,4 +1,4 @@
-function carousel() {
+function onLoad(){
     //화면객체
     let slideshow = document.querySelector(".slideshow");
     let slideshow_slides = document.querySelector(".slideshow_slides");
@@ -91,5 +91,67 @@ function carousel() {
             event.preventDefault();
             gotoslide(i);
         });
+    }
+
+
+    //패턴검색
+    const idPattern = /^[\w]{3,}$/; //[\w]는 영문자, 숫자, _만 입력 가능 {3,} 3글자이상가능
+    const pwdPattern =/^[\w]{6,10}$/; //영문자와 숫자, _ 6~10  
+    const namePattern =/^[가-힣]{2,4}|[A-Z]{1}[a-zA-Z\x20]{1,19}$/; //한글 2~4글자,영문자 2-20 첫글자는대문자 공백가능
+    const emailPattern =/^[a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/;
+    const mobilePattern =/^010-(?:[\d]{3}|[\d]{4})-[\d]{4}$/; //\d 숫자만가능
+    
+    //객체찾기
+    const inputID = document.querySelector("#userid"); 
+    const inputPW1 = document.querySelector("#pwd1"); 
+    const inputPW2 = document.querySelector("#pwd2"); 
+    const inputName = document.querySelector("#name"); 
+    const inputEmail= document.querySelector("#useremail"); 
+    const inputMobile= document.querySelector("#pho");
+    const myform= document.querySelector(".myform");
+
+     //이벤트리스너등록및 핸들러처리
+    inputID.addEventListener("blur",()=>validate(inputID, idPattern, "영문자, 숫자, _만 입력 가능" ));
+    inputPW1.addEventListener("blur",()=>validate(inputPW1,pwdPattern, "영문자와 숫자, _ 6~10" ));
+    inputPW2.addEventListener("blur",()=>{
+        validate(inputPW2,pwdPattern, "영문자와 숫자, _ 6~10" );
+        if(inputPW1.value !== inputPW2.value){
+            inputPW2.nextSibling.textContent ="패스워드가 일치하지 않음";
+            inputPW2.nextSibling.style.color ="red";
+            inputPW1.value="";
+            inputPW2.value="";
+            inputPW1.focus(); 
+            return; 
+        }
+    });
+    inputName.addEventListener("blur",()=>validate(inputName,namePattern, "한글 2~4글자,영문자 2-10 첫글자는대문자 공백가능" ));
+    inputEmail.addEventListener("blur",()=>validate(inputEmail,emailPattern, "이메일형식 안맞음" ));
+    inputMobile.addEventListener("blur",()=>validate(inputMobile,mobilePattern, "모바일전화번호형식이 안맞음" ));
+
+     //폼 이벤트등록및 핸들러처리
+     myform.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        validate(inputID, idPattern, "영문자, 숫자, _만 입력 가능" );
+        validate(inputPW1,pwdPattern, "영문자와 숫자, _ 6~10" );
+        validate(inputPW2,pwdPattern, "영문자와 숫자, _ 6~10" );
+        validate(inputName,namePattern, "한글 2~4글자,영문자 2-10 첫글자는대문자 공백가능" );
+        validate(inputEmail,emailPattern, "이메일형식 안맞음" );
+        validate(inputMobile,mobilePattern, "모바일전화번호형식이 안맞음" );
+        alert("서버로 전송하겠습니다.");
+        myform.submit();  
+    });
+
+    //핸들러처리기능    
+    function validate(userInput, pattern, message ){
+        if(userInput.value.match(pattern)){
+            userInput.nextSibling.innerHTML = "성공";
+            userInput.nextSibling.style.color ="blue";
+           }else{
+            userInput.nextSibling.innerHTML = message;
+            userInput.nextSibling.style.color ="red";
+            userInput.value = "";
+            userInput.focus();
+            return; 
+           }
     }
 }
